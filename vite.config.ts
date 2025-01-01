81a99618480f6aadb5791f41486db723fd50d804
Li <3454515879@qq.com>
2025-01-02 01:07:31 +0800

    安装配置好 Element Plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path'; // 确保这一行正常
+
+// 配置resolve 路径
+import { resolve } from 'path';
+
+// 配置Element Plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
       'assets': resolve(__dirname, './src/assets'),
     }
-  }
+  },
 });
